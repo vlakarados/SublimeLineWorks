50e723a26727de953edeb05228e6634d468a0810
Sergey Telshevsky <vlakarados@gmail.com>
2014-03-13 08:52:38 +0200

    Add Multiline-to-single line conversion

--- LineWorks.py
+++ LineWorks.py
@@ -1,8 +1,20 @@
-import sublime, sublime_plugin, subprocess
+import sublime, sublime_plugin, subprocess, re
+
+# singlify shamelessly ripped from singleline package
 
 class LineworksDeleteCommand(sublime_plugin.WindowCommand):
     def run(self, **args):
         self.window.show_input_panel("Which lines to delete?", "", Lineworks.delete, None, None)
+
+
+class LineworksMultiToSingleCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        for region in reversed(self.view.sel()):
+
+            text = self.view.substr(region)
+
+            text = Lineworks.singlify(text)
+            self.view.replace(edit, region, text)
 
 class Lineworks(sublime_plugin.TextCommand):
     def delete(str):
@@ -18,9 +30,15 @@ class Lineworks(sublime_plugin.TextCommand):
             print(line)
             sublime.active_window().active_view().run_command("lineworks_delete_line", {"line_n": i})
 
+    def singlify(text):
+        m = re.search('^(?P<leading>\s*).*$', text.split('\n')[0])  # grab leading spacing
+        single = m.group('leading') + ' '.join([x.strip() for x in text.split('\n')])
+        return single
+
 
 class LineworksDeleteLineCommand(sublime_plugin.TextCommand):
     def run(self, edit, line_n):
         aview = sublime.active_window().active_view()
         line_region = aview.line(aview.text_point(line_n,0))
         aview.erase(edit, line_region)
+
